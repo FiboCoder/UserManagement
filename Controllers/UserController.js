2c7fa8ce5cd7247588ee1f1f2fa6fadfcd86749d
Allan David Lopes Pereira <102700873+FiboCoder@users.noreply.github.com>
2022-05-18 06:44:34 -0300

    Update 18/05/2022

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,11 +1,67 @@
 class UserController{
 
-    constructor(formId, tableId){
+    constructor(formIdCreate, formIdUpdate, tableId){
 
-        this.formEl = document.getElementById(formId);
+        this.formEl = document.getElementById(formIdCreate);
+        this.formUpdateEl = document.getElementById(formIdUpdate);
         this.tableEl = document.getElementById(tableId);
 
         this.onSubmit();
+        this.onEdit();
+    }
+
+    onEdit(){
+
+        document.querySelector("#box-user-update .btn-cancel").addEventListener("click", e=>{
+
+            this.showPanelCreate();
+        });
+
+        this.formUpdateEl.addEventListener('submit', event =>{
+
+            event.preventDefault();
+
+            let btn = this.formUpdateEl.querySelector("[type=submit]");
+
+            btn.disable = true;
+
+            let values = this.getValues(this.formUpdateEl);
+
+            let index = this.formUpdateEl.dataset.trIndex;
+
+            let tr = this.tableEl.rows[index];
+
+            let userOld = JSON.parse(tr.dataset.user);
+
+            let result = Object.assign({}, userOld, values);
+
+            if(values.photo) result._photo = userOld._photo;
+
+            tr.dataset.user = JSON.stringify(result);
+
+            tr.tr.innerHTML = 
+            `<td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${result._name}</td>
+            <td>${result._email}</td>
+            <td>${(result._admin)? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(values._register)}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>`;
+
+            this.addEventsTR(tr);
+
+            this.updateCount();
+
+            btn.disabled = false;
+
+            this.formUpdateEl.reset();
+
+            this.showPanelCreate();
+
+
+        });
     }
 
     onSubmit(){
@@ -18,7 +74,9 @@ class UserController{
 
             btn.disabled = true;
 
-            let values = this.getValues();
+            let values = this.getValues(this.formEl);
+
+            if(!values) return false;
 
             this.getPhoto().then(
                 (content)=>{
@@ -76,11 +134,18 @@ class UserController{
         
     }
 
-    getValues(){
+    getValues(formEl){
 
         let user = {};
+        let isValid = true;
 
-        [...this.formEl.elements].forEach(function(field, index){
+        [...formEl.elements].forEach(function(field, index){
+
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+            }
 
             if(field.name === "gender"){
     
@@ -96,6 +161,11 @@ class UserController{
                 user[field.name] = field.value;
             }
         });
+
+        if(!isValid){
+
+            return false;
+        }
         
         return new User(
             user.name, 
@@ -112,18 +182,95 @@ class UserController{
 
         let tr = document.createElement('tr');
 
+        tr.dataset.user = JSON.stringify(dataUser);
+
         tr.innerHTML = 
         `<td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
         <td>${dataUser.email}</td>
         <td>${(dataUser.admin)? 'Sim' : 'Não'}</td>
-        <td>${dataUser.birth}</td>
+        <td>${Utils.dateFormat(dataUser.register)}</td>
         <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+          <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
           <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>`;
 
+        this.addEventsTR(tr)
+
         this.tableEl.appendChild(tr);
+
+        this.updateCount();
+    }
+
+    addEventsTR(tr){
+
+        tr.querySelector(".btn-edit").addEventListener('click', e=>{
+
+            let json = JSON.parse(tr.dataset.user);
+
+            form.dataset.trIndex = tr.sectionRowIndex;
+
+            for(let name in json){
+
+                let field = this.formUpdateEl.querySelector("[name=" + name.replace("_", "") + "]");
+
+                if(field){
+
+
+                    switch(field.type){
+
+                        case 'file':
+                            continue;
+                            break;
+
+                        case 'radio':
+                            field = this.formUpdateEl.querySelector("[name=" + name.replace("_", "") + "][value" + json[name] + "]");
+                            break;
+
+                        case 'checkbox':
+                            field.checked = json[name];
+                        break;
+
+                        default:
+                            field.value = json[name];
+
+                    }
+                }
+            }
+
+            this.formUpdateEl.querySelector(".photo").src = json._photo;
+
+            this.showPanelUpdate();
+        });
+    }
+
+    showPanelCreate(){
+
+        document.querySelector("#box-user-create").style.display = 'block';
+        document.querySelector("#box-user-update").style.display = 'none';
+    }
+
+    showPanelUpdate(){
+
+        document.querySelector("#box-user-create").style.display = 'none';
+        document.querySelector("#box-user-update").style.display = 'block';
+    }
+
+    updateCount(){
+
+        let numberUsers = 0;
+        let numberAdmin = 0;
+        [...this.tableEl.children].forEach(tr=>{
+
+            numberUsers++;
+
+            let user = JSON.parse(tr.dataset.user);
+            if(user._admin) numberAdmin++;
+
+        });
+
+        document.querySelector("#number-users").innerHTML = numberUsers;
+        document.querySelector("#number-users-admin").innerHTML = numberAdmin;
     }
 
 }
